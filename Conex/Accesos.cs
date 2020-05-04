0d19217d2bbc17fd8c810175c2dc66bd8d3a340e
ArcangelMiguel <miguelivanoff@hotmail.com>
2020-05-04 17:11:41 -0300

    Cambio de datos en la Conexion

--- Conex/Accesos.cs
+++ Conex/Accesos.cs
@@ -12,7 +12,7 @@ namespace Conexiones
     {
         public static DataSet datos(string sql)
         {
-            MySqlConnection conn = new MySqlConnection("server=localhost; database=ozogvial; Uid=root; pwd='MaImEs3321';");
+            MySqlConnection conn = new MySqlConnection("server=192.168.0.103; database=ozogvial; Uid=miguel; pwd='1234';");
             conn.Open();
 
             DataSet DS = new DataSet();
@@ -26,7 +26,7 @@ namespace Conexiones
 
         public static MySqlConnection UnaConexion()
         {
-            MySqlConnection conx = new MySqlConnection("server=localhost; database=ozogvial; Uid=root; pwd='MaImEs3321';");
+            MySqlConnection conx = new MySqlConnection("server=192.168.0.103; database=ozogvial; Uid=miguel; pwd='1234';");
             conx.Open();
 
             return conx;
